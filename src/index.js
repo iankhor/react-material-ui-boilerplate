64952c6af519171d19a70ab590bf675c9c81c29f
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-07 18:07:40 +1100

    updated file/folder structure with routes

--- src/index.js
+++ src/index.js
@@ -2,26 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter , Match, Miss } from 'react-router'
 
-//components
-import NotFound from './components/shared/NotFound'
-import App from './components/App';
-import PageOne from './components/PageOne'
+// Routes
+import Routes from './components/shared/Routes'
 
 // Material UI functions
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin()
 
 
-
 const Root = () => {
   return (
-    <BrowserRouter>
       <div>
-        <Match exactly pattern="/" component={App} />  
-        <Match exactly pattern="/PageOne" component={PageOne} />  
-        <Miss component={NotFound} />  
+        <Routes />
       </div>
-    </BrowserRouter>
   )
 }
 
