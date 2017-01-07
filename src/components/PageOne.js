0d5ede9e916e6266e4aa1fc2fca6a929308dfad4
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-07 18:20:01 +1100

    updated JSON onscreen debugger

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 import App from './App'
+import JSONDebugger from '../utils/JSONDebugger'
+
+// example JSON data
+const exampleJSONData = {
+    data1: 'data1',
+    data2: 'data2',
+    data3: 123
+}
 
 //material-ui components
 import { TextField } from 'material-ui/';
@@ -13,6 +21,9 @@ class PageOne extends Component {
                     hintText="Type something here"
                     floatingLabelText="This is a text field child from PageOne component"
                 />
+                <p> Below is an example of the JSON debugger</p>
+                <JSONDebugger json={exampleJSONData} />
+
             </App> 
         )
     }
--- src/utils/JSONDebugger.js
+++ src/utils/JSONDebugger.js
@@ -4,8 +4,8 @@ class JSONDebugger extends Component {
     render(){
         return(
               <pre>
-                <h2> The Kick Ass On Screen Debugger React Component</h2>
-                {JSON.stringify(this.props.data, null, 2)}
+                <h2>JSON Debugger</h2>
+                {JSON.stringify(this.props.json, null, 2)}
               </pre>
         )
     }
